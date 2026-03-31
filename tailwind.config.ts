87550156e7e79d277ddef95067a9dc4e8b29cb7e
mayurtronix@gmail.com <mayurtronix@gmail.com>
2026-03-31 22:34:37 +0530

    frontend issues for light gray tetx is fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#4F46E5',
+        secondary: '#F97316',
+        accent: '#10B981',
+        dark: '#0a0a0a',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
